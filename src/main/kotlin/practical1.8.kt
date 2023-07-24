f24d74f5f1bc813201dee3aa3257b236c55de31a
student <student@gnu.ac.in>
2023-07-24 10:43:58 +0530

    commit

--- src/main/kotlin/practical1.8.kt
+++ src/main/kotlin/practical1.8.kt
@@ -1,3 +1,23 @@
-fun main(){
-    println("hello")
+open class person(var mobileno:Int){
+    var address:String=""
+
+    constructor(mno:Int,add:String):this(mno){
+        this.address=add
+        this.mobileno=mno
+    }
+
 }
+
+class student(var enno:Int,var name:String)
+{
+    constructor(): this(82,"dev"){
+    }
+}
+fun main(){
+    var s1=student()
+
+    println(s1.enno)
+    println(s1.name)
+
+}
+
